97aea91c790091d0e0d0b655de07549940d2c649
DanielRubiesSmx2a <danismx2a@gmail.com>
2024-04-24 02:44:51 -0400

    cambios para docs larv

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -9,7 +9,13 @@ class DocumentController extends Controller
      // Método para obtener todos los documentos
      public function index()
      {
-         return Documents::all();
+   // Obtener el tipo de documento "contracts"
+   $type = 'contracts'; // Puedes ajustar este valor según tus necesidades
+
+   // Obtener todos los documentos filtrados por el tipo de documento
+   $documents = Document::where('type_documents', $type)->get();
+
+   return response()->json($documents);
      }
 
      // Método para almacenar un nuevo documento
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\InvoiceController;
+use App\Http\Controllers\DocumentController;
 
 use App\Http\Controllers\LoginController;
 use Illuminate\Http\Request;
