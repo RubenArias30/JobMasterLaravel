e704d4cfceaedd0b170c2d2bfec22e3e1f836806
DanielRubiesSmx2a <danismx2a@gmail.com>
2024-05-06 07:52:30 -0400

    PROFILECHANGES

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -12,23 +12,28 @@ class ProfileController extends Controller
     {
         $this->middleware('auth:api');
     }
+
     public function getProfile(Request $request)
     {
-   
-        // Obtener el ID del usuario autenticado
-        $userId = auth()->user()->id;
-        // Buscar el perfil del empleado asociado al usuario autenticado
-        $employee = Employees::where('users_id', $userId)->first();
-        dd($userId);
-        dd($employee);
+        try {
+            // Obtener el ID del usuario autenticado
+            $userId = Auth::user()->id;
 
-        // Verificar si se encontró el perfil del empleado
-        if ($employee) {
-            // Si se encontró, devolver los datos del perfil del empleado
+            // Buscar el perfil del empleado asociado al usuario autenticado
+            $employee = Employees::where('users_id', $userId)->with('addresses','users')->first();
+
             return response()->json($employee);
-        } else {
-            // Si no se encontró el perfil, retornar un mensaje de error o un código de estado HTTP adecuado
-            return response()->json(['error' => 'Perfil de empleado no encontrado'], 404);
+            // // Verificar si se encontró el perfil del empleado
+            // if ($employee) {
+            //     // Si se encontró, devolver los datos del perfil del empleado
+            //     return response()->json(['success' => true, 'profile' => $employee]);
+            // } else {
+            //     // Si no se encontró el perfil, retornar un mensaje de error
+            //     return response()->json(['success' => false, 'message' => 'Perfil de empleado no encontrado'], 404);
+            // }
+        } catch (\Exception $e) {
+            // Manejar cualquier excepción que pueda ocurrir
+            return response()->json(['success' => false, 'message' => 'Ha ocurrido un error al obtener el perfil del empleado'], 500);
         }
     }
 
